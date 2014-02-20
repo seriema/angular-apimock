4af60a1c6f5c3ca063875ad88600fd5f506d2cc4
John-Philip Johansson <seriema@gmail.com>
2014-02-20 22:54:50 +0100

    Minor beutifying

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,5 +4,4 @@ angular.module('angularOfflineApp', ['offlineMode'])
 	.run(function (httpInterceptor) {
 		httpInterceptor.config.OFFLINE_DATA_PATH = '/offline_data';
 		httpInterceptor.config.API_PATH = '/api';
-	}
-);
+	});
