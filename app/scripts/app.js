e7721b6fd2083d895728e2cc260061e14e20fd39
John-Philip Johansson <seriema@gmail.com>
2014-02-20 20:13:28 +0100

    First working version

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,63 +1,8 @@
-function getUrlParameterByName(name) {
-	name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-	var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
-		results = regex.exec(location.search);
-	return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
 
-var OFFLINE = getUrlParameterByName('offline') === 'true';
-var JS_ANGULAR_FOLDER = '/bower_components/angular-mocks/';
-var JSON_SERVER_URL = '/offline_data/';
+angular.module('angularOfflineApp', ['offlineMode'])
 
-function demand(file) {
-	var result;
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4) {
-			result = xhr.responseText;
-		}
-	};
-	xhr.open('GET', file, false);
-	xhr.send(null);
-
-	return result;
-}
-
-angular.module('angularOfflineApp', ['offlineHttpInterceptor'])
-/*
-	.config(['$provide', function ($provide) {
-		if (OFFLINE) {
-			var se = document.createElement('script');
-			se.type = 'text/javascript';
-			se.text = demand(JS_ANGULAR_FOLDER+'angular-mocks.js');
-			document.getElementsByTagName('head')[0].appendChild(se);
-
-			$provide.decorator('$httpBackend', angular.mock.e2e.$httpBackendDecorator);
-		}
-	}])
-*/
-/*
-	.config(['$httpProvider', function ($httpProvider) {
-		$httpProvider.interceptors.push(function() {
-			return {
-				'request': function(config) {
-					// same as above
-				},
-
-				'response': function(response) {
-					// same as above
-				}
-			};
-		});
-	}])
-*/
-	.run(['$injector', function ($injector) {
-		if (OFFLINE) {
-//			var $httpBackend = $injector.get('$httpBackend');
-//			var $httpProvider = $injector.get('$httpProvider');
-
-
-//			$httpBackend.when('GET', '/api/people/pikachu').respond(demand(JSON_SERVER_URL+'people/pikachu.json'));
-//			$httpBackend.when('GET', new RegExp('/api/ProductSearch(\.*)')).respond(demand(JSON_SERVER_URL+'get.productsearch.json'));
-		}
-	}]);
+	.run(function (httpInterceptor) {
+		httpInterceptor.config.OFFLINE_DATA_PATH = '/offline_data/';
+		httpInterceptor.config.API_PATH = 'api';
+	}
+);
--- app/scripts/services/offlinehttpinterceptor.js
+++ app/scripts/services/offlinehttpinterceptor.js
@@ -1,17 +1,33 @@
 'use strict';
 
-angular.module('offlineHttpInterceptor', [])
-  .config(function ($httpProvider) {
+angular.module('offlineMode', [])
+	.config(function ($httpProvider) {
+		$httpProvider.interceptors.push('httpInterceptor');
+	})
+	.factory('httpInterceptor', function ($q) {
+		function getUrlParameterByName(name) {
+			name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+			var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+				results = regex.exec(location.search);
+			return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+		}
 
-	$httpProvider.interceptors.push(function() {
-		return {
-			'request': function(config) {
-				if (config.url.indexOf('/api') > -1) {
-					config.url = '/offline_data/people/pikachu.json';
-				}
-				return config;
-				// same as above
-			}
+		var OFFLINE = getUrlParameterByName('offline') === 'true';
+		var _config = {
+			OFFLINE_DATA_PATH: 'lol',
+			API_PATH: 'rooofl'
 		};
-	});
-  });
+
+		return {
+			request: function(config) {
+				var parts = config.url.indexOf('/') === 0 ? config.url.substr(1, config.url.length).split('/') : config.url.split('/');
+
+				if (OFFLINE && parts.splice(0, 1)[0] === _config.API_PATH) {
+					config.url = _config.OFFLINE_DATA_PATH + parts.join('/') + '.json';
+				}
+				return config || $q.when(config);
+			},
+			config: _config
+		};
+	}
+);
