96e62261123dcfa4d883b5356f2f40f73905c40f
John-Philip Johansson <seriema@gmail.com>
2014-02-22 17:15:24 +0100

    Indentation on main.js

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,21 +2,25 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('angularOfflineApp'));
+	// load the controller's module
+	beforeEach(module('angularOfflineApp'));
 
-  var MainCtrl,
-    scope;
+	var MainCtrl,
+		scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
+	// Initialize the controller and a mock scope
+	beforeEach(inject(function ($controller, $rootScope) {
+		scope = $rootScope.$new();
+		MainCtrl = $controller('MainCtrl', {
+			$scope: scope
+		});
+	}));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+	it('should have "unknwon" as name when starting', function () {
+		expect(scope.name).toBe('unknown');
+	});
+
+	it('should have "null" as command when starting', function () {
+		expect(scope.command).toBe('null');
+	});
 });
