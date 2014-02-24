f89fdda136f3ec517b29b4d1bccde820ae911122
Joel Lundberg <joel@redhorn.se>
2014-02-25 00:49:35 +0100

    New dev branch for GH pages

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('angularOfflineApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularOfflineApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
