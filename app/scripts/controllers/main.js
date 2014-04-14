68e4220de72a5a1081d0b6b65aa3e0195c7a3310
John-Philip Johansson <seriema@gmail.com>
2014-04-14 20:17:06 +0200

    Remove unused code in MainCtrl.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,4 @@
 
 angular.module('angularApimockApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
