7660a08c8336c865ce45ecdf55d2fd8088ff938d
Joel Lundberg <joel@redhorn.se>
2014-04-16 09:27:03 +0200

    Updates

--- scripts/a3f46c45.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("angularApimockApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("angularApimockApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
