04a6a59c1edcb8edc142358fcc0c71b91590ca28
John-Philip Johansson <seriema@gmail.com>
2014-04-17 12:21:35 +0200

    Fix paths for simple demo.

--- app/scripts/controllers/demo-simple.js
+++ app/scripts/controllers/demo-simple.js
@@ -1,12 +1,20 @@
 'use strict';
 
 angular.module('angularApimockApp')
+
+  .config(function($httpInterceptorProvider) {
+    $httpInterceptorProvider.config({
+      mockDataPath: 'mock_data',
+      apiPath: 'api'
+    })
+  })
+
   .controller('DemoSimpleCtrl', function ($scope, $http) {
     $scope.players = [];
     $scope.alerts = [];
 
     $scope.getHighscore = function() {
-      $http.get('/api/scores/top')
+      $http.get('api/scores/top')
         .success(function(data) {
           $scope.players = data;
         })
