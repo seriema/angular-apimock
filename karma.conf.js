9c03895dfbc9b8e596449c73cde9257e6cfcd2ca
John-Philip Johansson <seriema@gmail.com>
2014-04-19 12:49:23 +0200

    Enable Firefox for testing. #6

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
