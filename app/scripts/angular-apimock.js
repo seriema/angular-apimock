81cf7258363cfbc744aa6a8ebfd458bf9e72e172
John-Philip Johansson <seriema@gmail.com>
2014-04-19 12:19:20 +0200

    Refactor apiMock service to support issue #5.
    Methods can now be overridden in .config() phase.

--- app/scripts/angular-apimock.js
+++ app/scripts/angular-apimock.js
@@ -4,51 +4,63 @@ angular.module('apiMock', [])
   $httpProvider.interceptors.push('httpInterceptor');
 })
 
-.factory('apiMock', function($location) {
-  return {
-    shouldReplace: function (req, apiPath) {
-      return req.url.indexOf(apiPath) === 0;
-    },
+.provider('apiMock', function() {
+  var $location;
 
-    replacePath: function (req, apiPath, mockDataPath) {
-      var path = req.url.substring(apiPath.length);
-      req.url = mockDataPath + path + '.' + req.method.toLowerCase() + '.json';
-    },
+  function shouldReplace(req) {
+    return req.url.indexOf(config.apiPath) === 0;
+  }
 
-    isMocking: function() {
-      var regex = /apimock/i;
-      var found = false;
+  function replacePath(req) {
+    var path = req.url.substring(config.apiPath.length);
+    req.url = config.mockDataPath + path + '.' + req.method.toLowerCase() + '.json';
+  }
 
-      angular.forEach($location.search(), function(value, key) {
-        if (regex.test(key)) {
-          // Update $location object with primitive boolean compatibility in case if string type.
-          if (value === true || angular.lowercase(value) === 'true') {
-            found = true;
-            $location.search(key, null);
-            $location.search('apimock', true);
-          }
+  function isMocking() {
+    var regex = /apimock/i;
+    var found = false;
+
+    angular.forEach($location.search(), function(value, key) {
+      if (regex.test(key)) {
+        // Update $location object with primitive boolean compatibility in case if string type.
+        if (value === true || angular.lowercase(value) === 'true') {
+          found = true;
+          $location.search(key, null);
+          $location.search('apimock', true);
         }
-      });
+      }
+    });
 
-      return found;
-    }
-  };
-})
+    return found;
+  }
 
-.provider('httpInterceptor', function() {
   var config = {
     mockDataPath: '/mock_data',
-    apiPath: '/api'
+    apiPath: '/api',
+    shouldReplace: shouldReplace,
+    replacePath: replacePath,
+    isMocking: isMocking
   };
+
+  function ApiMock(_$location) {
+    angular.extend(this, config);
+    $location = _$location;
+  }
 
   this.config = function (options) {
     angular.extend(config, options);
   };
 
+  this.$get = function ($location) {
+    return new ApiMock($location);
+  };
+})
+
+.provider('httpInterceptor', function() {
   function HttpInterceptor($q, apiMock) {
     this.request = function (req) {
-      if (req && apiMock.isMocking() && apiMock.shouldReplace(req, config.apiPath)) {
-        apiMock.replacePath(req, config.apiPath, config.mockDataPath);
+      if (req && apiMock.isMocking() && apiMock.shouldReplace(req)) {
+        apiMock.replacePath(req);
       }
 
       return req || $q.when(req);
