2bf2a7ef0d5257c7f6a1e96be09f74bc0cc85646
John-Philip Johansson <seriema@gmail.com>
2014-04-27 10:38:43 +0200

    Add base for the BIG Magic the Gathering demo.

---
+++ app/scripts/controllers/demo-big.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('angularApimockApp')
+
+  .config(function(httpInterceptorProvider) {
+    httpInterceptorProvider.config({
+      mockDataPath: 'mock_data',
+      apiPath: 'api'
+    });
+  })
+
+  .controller('DemoBigCtrl', function ($scope, $http) {
+    $scope.players = [];
+    $scope.alerts = [];
+
+    $scope.getCards = function() {
+      $http.get('api/magic-game/cards/all')
+        .success(function(data) {
+          $scope.players = data;
+        })
+        .error(function() {
+          var msg = 'No connection :( Now add ?apimock=true to the browser addressbar and try again ;)';
+          $scope.alerts.push({ type: 'danger', text: msg });
+        });
+    };
+
+    $scope.closeAlert = function(index) {
+      $scope.alerts.splice(index, 1);
+    };
+  });
