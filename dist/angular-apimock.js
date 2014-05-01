fadff4cf704ac4aea5f874a09276e27b77a8ff87
John-Philip Johansson <seriema@gmail.com>
2014-05-01 13:12:02 +0200

    Update comments.

--- dist/angular-apimock.js
+++ dist/angular-apimock.js
@@ -2,34 +2,46 @@
  * Licensed with MIT
  * Made with ♥ from Seriema + Redhorn */
 /* Create the main module, 'apiMock'. It's the one that needs to be included in
-   other projects. */
+   your projects. E.g. 'angular.module('myApp', ['apiMock'])''. You don't need
+   to do anything else, but you can configure the paths for api-calls and mock
+   data by calling '.config(apiMockProvider)'. */
 angular.module('apiMock', []).config([
   '$httpProvider',
   function ($httpProvider) {
-    /* This is where the magic happens. Configure $httpProvider to use our
-   httpInterceptor on all calls. It's what allows us to do automatic routing. */
+    /* This is where the magic happens. Configure $http to use our httpInterceptor
+   on all calls. It's what allows us to do automatic routing. */
     $httpProvider.interceptors.push('httpInterceptor');
   }
 ]).provider('apiMock', function () {
-  /* This is the default implementation of apiMock. It's driven by the maintainers
-   goals, but can be overriden in apiMock.config(). The members are:
-   'isApiPath' function: takes a `request` object and decides if the path
-                         should be rerouted. Default: Checks the requested
-                         URL for `apiPath` and returns true if it's the
-                         beginning of the path.
-   'reroutePath' function: takes a `request` object and replaces the URL with
-                           the routed path. Default: Simply replaces `apiPath`
-                           with `mockDataPath`, but adds the HTTP verb and
-                           `.json` at the end of the path. A GET request to
-                           '/api/user/5' turns into '/api/user/5.get.json'.
-   'isGlobalMock' function: decides if mocking is enabled. Default: Checks
-                           `$location` for a `apimock` variable and that it's set
-                           to `true`.
-   'isLocalMock' function: takes a `request` object and decides if mocking is
-                           overriden. Default: checks the request object for a
-                           `apimock` property set to `true`.
+  /* This is the Provider for apiMock. It's used by 'httpInterceptor' to determine
+   when and how to do the mocking (routing).
+
+   Config options:
    'mockDataPath' string: the path to be rerouted to. Default: '/mock_data'.
-   'apiPath' string: the path to be rerouted from. Default: '/api'. */
+   'apiPath' string: the path to be rerouted from. Default: '/api'.
+
+   Public interface:
+   'shouldMock' method: takes a 'request' object and decides if mocking should
+                        be done on this request. It checks global and local
+                        apiMock flags to see if it should mock. It also checks
+                        the request URL if it starts with 'apiPath'.
+    'doMock' method: takes a 'request' object and modifies it. Specifically it
+                     changes the request URL to the 'mockDataPath'. It also
+                     appends the HTTP verb and .json to the path. Note: query
+                     parameters (e.g. ?search=banana) are stripped. A GET
+                     request to '/api/user/5?option=full' turns into
+                     '/mock_data/user/5.get.json'.
+
+   Private members:
+   '_isApiPath' method: takes a 'request' object and checks the requested
+                        URL for `apiPath` and returns true if it's the
+                        beginning of the path.
+   '_isGlobalMock' method: decides if mocking is enabled by checking
+                           '$location' for query parameter 'apimock' and that
+                           it's set to 'true' (or empty). E.g. '?apimock=true'.
+   '_isLocalMock' method: takes a 'request' object and decides if mocking is
+                          overriden by checking the request object for a
+                          'apimock' property set to 'true'. */
   var mockDataPath = '/mock_data';
   var apiPath = '/api';
   var $location;
@@ -92,7 +104,9 @@ angular.module('apiMock', []).config([
   'apiMock',
   function ($q, apiMock) {
     /* The main service. Is jacked in as a interceptor on $http so it gets called
-   on every http call. This allows us to do our magic. */
+   on every http call. This allows us to do our magic. It uses the provider
+   'apiMock' to determine if a mock should be done, then do the actual mocking.
+*/
     this.request = function (req) {
       if (req && apiMock.shouldMock(req)) {
         apiMock.doMock(req);
