6cf826cc135deae83f6233f3e2646b545eebd6c5
John-Philip Johansson <seriema@gmail.com>
2014-05-11 15:27:38 +0200

    Release v0.1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,22 @@ module.exports = function (grunt) {
       dist: 'dist'
     },
 
+		bump: {
+			options: {
+				files: ['package.json'],
+				updateConfigs: [],
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['-a'], // '-a' for all files
+				createTag: true,
+				tagName: 'v%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: true,
+				pushTo: 'master',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+			}
+		},
+
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       js: {
