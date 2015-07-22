bb77dc3a63f3da19858a38ecda66e2f81756c318
John-Philip Johansson <seriema@gmail.com>
2015-07-22 17:05:53 +0200

    Add missing angular-apimock reference to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-bootstrap/ui-bootstrap.js',
+      'app/bower_components/angular-apimock/dist/angular-apimock.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
