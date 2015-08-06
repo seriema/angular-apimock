218e305f5e1cbaeb1e15b0980498dc9145a45fa7
John-Philip Johansson <seriema@gmail.com>
2015-08-06 13:41:48 +0200

    chore(karma): change port to default
    8080 is way too common.

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
 		exclude: [],
 
 		// web server port
-		port: 8080,
+		port: 9876, // default
 
 		// level of logging
 		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
